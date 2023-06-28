d0cdb9d38287c0d8691bc2c1527b25399c586a73
Marcos Vicente Cruz <marcoscruz@sigmasysengenharia.com.br>
2023-06-27 21:29:15 -0300

    Adicionada a geração de arquivos de teste.

--- psim.cpp
+++ psim.cpp
@@ -50,11 +50,51 @@ ADDAPI void ADDCALL SimulationStep(
 
 	decltype(cpt)::power_vector i;
 
-	i[0] = in[2];
-	i[1] = in[3];
-	i[2] = in[4];
+	i[0] = in[3];
+	i[1] = in[4];
+	i[2] = in[5];
 
-	auto r = cpt.feed(u, i);
+	const auto r = cpt.feed(u, i);
+
+	out[0] = r.u[0];
+	out[1] = r.u[1];
+	out[2] = r.u[2];
+
+	out[3] = r.i[0];
+	out[4] = r.i[1];
+	out[5] = r.i[2];
+
+	out[6] = r.p[0];
+	out[7] = r.p[1];
+	out[8] = r.p[2];
+
+	out[9] = r.P[0];
+	out[10] = r.P[1];
+	out[11] = r.P[2];
+
+	out[12] = r.w[0];
+	out[13] = r.w[1];
+	out[14] = r.w[2];
+
+	out[15] = r.W[0];
+	out[16] = r.W[1];
+	out[17] = r.W[2];
+
+	out[18] = r.U[0];
+	out[19] = r.U[1];
+	out[20] = r.U[2];
+
+	out[21] = r.ia[0];
+	out[22] = r.ia[1];
+	out[23] = r.ia[2];
+
+	out[24] = r.ir[0];
+	out[25] = r.ir[1];
+	out[26] = r.ir[2];
+
+	out[27] = r.iv[0];
+	out[28] = r.iv[1];
+	out[29] = r.iv[2];
 }
 
 
@@ -102,6 +142,9 @@ ADDAPI void ADDCALL SimulationEnd(const char *szId, void ** reserved_UserData, i
 #endif
 
 #else
+
+#include <fstream>
+
 /** Test routine so this file can be used outside PSIM. **/
 int main(int argc, char **argv) {
 	static CPT<3, SAMPLE_RATE, FREQ> cpt;
@@ -109,10 +152,31 @@ int main(int argc, char **argv) {
 	decltype(cpt)::power_vector u;
 	decltype(cpt)::power_vector i;
 
-	u.fill(2);
-	i.fill(3);
+	std::ofstream file ("/tmp/.marcos-private/saida.csv", std::ofstream::out);
 
-	const auto r = cpt.feed(u, i);
+	file << "Va" << "\t" << "Vb" << "\t" << "Vc" << "\t";
+	file << "Ia" << "\t" << "Ib" << "\t" << "Ic" << "\t";
+	file << "pa" << "\t" << "pb" << "\t" << "pc" << "\t";
+	file << "Pa" << "\t" << "Pb" << "\t" << "Pc";
+	file << std::endl;
+
+	for (unsigned int o=0; o<2*SAMPLE_RATE/FREQ; o++) {
+		u[0] = 127 * sin(2 * M_PI * o / (SAMPLE_RATE/FREQ) );
+		u[1] = 127 * sin(2 * M_PI * o / (SAMPLE_RATE/FREQ) - 2*M_PI/3);
+		u[2] = 127 * sin(2 * M_PI * o / (SAMPLE_RATE/FREQ) + 2*M_PI/3);
+
+		i[0] =   1 * sin(2 * M_PI * o / (SAMPLE_RATE/FREQ) );
+		i[1] =   1 * sin(2 * M_PI * o / (SAMPLE_RATE/FREQ) - 2*M_PI/3);
+		i[2] =   1 * sin(2 * M_PI * o / (SAMPLE_RATE/FREQ) + 2*M_PI/3);
+
+		const auto r = cpt.feed(u, i);
+
+		file << r.u[0] << "\t" << r.u[1] << "\t" << r.u[2] << "\t";
+		file << r.i[0] << "\t" << r.i[1] << "\t" << r.i[2] << "\t";
+		file << r.p[0] << "\t" << r.p[1] << "\t" << r.p[2] << "\t";
+		file << r.P[0] << "\t" << r.P[1] << "\t" << r.P[2];
+		file << std::endl;
+	}
 
 	return 0;
 }
